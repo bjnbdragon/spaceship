ab5fa08cfe19d2618f994b9c311c268592196e03
bjnbdragon <nbdragon@gmail.com>
2021-08-26 18:17:46 +0800

    Modify file contents

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,7 @@ public class HelloWorld{
    public static void main(String args[]){
         System.out.println(“Hello World”);
    }
+   public void main1(){
+        System.out.println(“Hello World”);
+   }   
 }
